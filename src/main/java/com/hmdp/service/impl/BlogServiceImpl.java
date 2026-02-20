2f347ec954ca9d7502a44ca6582d3ce4c78feea9
tayhirro <tayhirro@gmail.com>
2026-02-20 11:45:36 +0800

    vault backup: 2026-02-20 11:45:36

--- src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -1,11 +1,11 @@
 package com.hmdp.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Blog;
 import com.hmdp.mapper.BlogMapper;
 import com.hmdp.service.IBlogService;
 import com.hmdp.utils.UserHolder;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -23,7 +23,7 @@ import static com.hmdp.utils.RedisConstants.BLOG_LIKED_KEY;
 public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IBlogService {
     
     @Autowired
-    private RedisTemplate<String, Object> redisTemplate;
+    private RedisTemplate<String, Object> stringRedisTemplate;
     
 
     @Override
@@ -35,8 +35,12 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
     public Result likeBlog(Long id){
         Long userId = UserHolder.getUser().getId();
         String key = BLOG_LIKED_KEY + id;
+        Double score = stringRedisTemplate.opsForZSet().score(key, userId.toString());
+        if (score == null){
+            stringRedisTemplate.opsForZSet().add(key, userId.toString(), System.currentTimeMillis());
+            return Result.ok("点赞成功");
+        }
 
-        
 
     }
 
